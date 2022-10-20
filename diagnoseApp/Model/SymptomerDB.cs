9c6e30a41119fa461a9027a50ce1968d056e4c46
Mohammad-Khir <s343988@oslomet.no>
2022-10-20 14:30:31 +0200

    La til main.html, main.js ,symptomerDB og symptomer.cs

---
+++ diagnoseApp/Model/SymptomerDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace diagnoseApp.Model
+{
+    public class SymptomerDB : DbContext
+    {
+        protected SymptomerDB(DbContextOptions<SymptomerDB> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Symptomer> Symptomers { get; set; }
+    }
+}
--- diagnoseApp/wwwroot/js/main.js
+++ diagnoseApp/wwwroot/js/main.js
@@ -1,30 +1,49 @@
-﻿$(function () {
-    hentAllePersoner();
-});
+﻿function diagnosere() {
+    
 
-function hentAllePersoner() {
-    $.get("person/hentAlle", function (personer) {
-        formaterPersoner(personer);
+    let ut = "pasienten har : " + $("input:radio[name=feber]:checked").val() +
+        "og har " + $("input:radio[name=hoste]:checked").val() +
+        "og har " + $("input:radio[name=hp]:checked").val() +
+        "og har " + $("input:radio[name=slapphet]:checked").val() +
+        "og har " + $("input:radio[name=tp]:checked").val();
+
+    ut += "<br/>og nedsatt på : ";
+    const miste = $("input:checkbox[name=miste]:checked");
+    for (let sans of miste) {
+        ut += sans.misteValue + "og ";
+    }
+
+    ut += "<br/>og andre symptomer  : ";
+    const andre = $("input:checkbox[name=andre]:checked");
+    for (let en of andre) {
+        ut += en.andreValue + "og ";
+    }
+
+    $("#result").html(ut);
+
+    const url = "/lagresymptomer";
+    $.post(url, symptomer, function () {
+        
     });
+
 }
 
-function formaterPersoner(personer) {
-    let ut = "<table class='table table-striped'>" +
-        "<tr>" +
-        "<th>Fornavn</th><th>Etternavn</th><th>Fødselsnr</th><th>Adresse</th><th>Tlf</th><th>Epost</th><th></th><th></th>" +
-        "</tr>";
-    for (let person of personer) {
-        ut += "<tr>" +
-            "<td>" + person.fornavn + "</td>" +
-            "<td>" + person.etternavn + "</td>" +
-            "<td>" + person.fodselsnr + "</td>" +
-            "<td>" + person.adresse + "</td>" +
-            "<td>" + person.tlf + "</td>" +
-            "<td>" + person.epost + "</td>" +
-            "<td> <a class='btn btn-primary' href='endre.html?id=" + kunde.id + "'>Endre</a></td>" +
-            "<td> <button class='btn btn-danger' onclick='slettKunde(" + kunde.id + ")'>Slett</button></td>" +
-            "</tr>";
-    }
-    ut += "</table>";
-    $("#personene").html(ut);
+function hentSymtomene() {
+    $.get("Symptom/HentAlle", function (symptomer) {
+        for (let symptom of symptomer) {
+            window.location.href = "result.html?id=" + symptom.id;
+        }
+    });
+} 
+
+function hentPersonen() {
+    $.get("Person/HentAlle", function (personer) {
+        for (let person of personer) {
+            window.location.href = "velkommen.html?id=" + person.id;
+        }
+    });
+} 
+
+function formaterResultat() {
+
 }
